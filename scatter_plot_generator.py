3a3c5e472affb62545377537d017acf9727616a2
Cristian Ponce <cponce@uncc.edu>
2020-08-15 22:12:24 -0400

    cleaned up code, wrote functions to avoid repetitive sections of code

--- scatter_plot_generator.py
+++ scatter_plot_generator.py
@@ -1,7 +1,6 @@
 import csv
 from Bio import SeqIO
 import matplotlib.pyplot as plt
-import statistics
 
 contig_file = "ChineseAmber.contigs.fasta"
 best_hit_file = "ChineseAmber_sorted.csv"
@@ -17,28 +16,28 @@ def extract_data(file_name):
         return extracted_data
 
 
-def plot_data(data_set_one, c='blue'):
+def plot_data(data_set_one, c='blue', line_name='insert line name here'):
     x = []
     y = []
     z = []
 
-    for entry in contig_data:
-        mbp_covered = int(entry[2]) / 1000000
+    for entry in data_set_one:
+        contig_size = int(entry[2]) / 1000000
         query_coverage = float(entry[1]) / 100
+        x.append(contig_size)
         y.append(query_coverage)
-        x.append(mbp_covered)
-        z.append(query_coverage * mbp_covered)
+        z.append(query_coverage * contig_size)
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
     csfont = {'fontname': 'Times New Roman'}
 
-    ax.scatter(z, y, x, marker='o', c=c)
-    plt.title("'Best hit' contig statistics plot", **csfont)
-    plt.ylabel('Query coverage percentage (0-1)', **csfont)
-    plt.xlabel('Covered nucleotides in Mbp', **csfont)
-    ax.set_zlabel('Contig size in Mbp', **csfont)
+    ax.scatter(x, y, z, marker='o', c=c)
+    plt.title(f"Size, query coverage, and nucleotide coverage in {line_name} contigs", **csfont)
+    plt.xlabel('Contig size (Mbp)', **csfont)
+    plt.ylabel('Query coverage % (0-1)', **csfont)
+    ax.set_zlabel('Nucleotides covered (Mbp)', **csfont)
     plt.show()
 
 
@@ -64,6 +63,5 @@ filtered_linkage_data = []
 for entry in linkage_data:
     filtered_linkage_data.append([entry[0], entry[1], contig_data.get(entry[0])])
 
-plot_data(filtered_best_hit_data)
-plot_data(filtered_linkage_data, c='red')
-
+plot_data(filtered_best_hit_data, line_name='C. Amber')
+plot_data(filtered_linkage_data, c='red', line_name='C. Amber')
