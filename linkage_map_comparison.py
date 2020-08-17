20d49ea7fc98dc519507c91122e480ef64ceaecc
cponce-uncc <cristianponce49@gmail.com>
2020-08-17 15:26:55 -0400

    Compares the contig order from the results of two seperate pairwise alignments

--- linkage_map_comparison.py
+++ linkage_map_comparison.py
@@ -1,8 +1,9 @@
-# NOTE: I only found a single contig (tig00002499) at the 70% threshold level. 15% yielded better results.
 import csv
 import difflib
 
-threshold_hits_file = 'sorted_test_plus_linkage.csv'
+threshold_hits_file_one = "tx2783_ChineseAmber_sorted.csv"
+threshold_hits_file_two = "tx463_ChineseAmber_sorted.csv"
+threshold_hits_file_three = 'Rio_ChineseAmber_sorted.csv'
 linkage_maps_file = 'ChineseAmber_Linkage_Map.csv'
 
 
@@ -26,58 +27,69 @@ def convert_to_csv(old_file_path, new_file_path):
 
 
 def get_common_contigs(list_one, list_two):
-    list_one = set(list_one)
-    list_two = set(list_two)
-    return list_one.intersection(list_two)
+    print('1', list_one)
+    print('2', list_two)
+    new_list_one = []
+    new_list_two = []
+
+    for entry in list_one:
+        if entry in list_two:
+            new_list_one.append(entry)
+
+    for entry in list_two:
+        if entry in list_one:
+            new_list_two.append(entry)
+
+    return new_list_one, new_list_two
 
 
-threshold_hits = list(open(threshold_hits_file, 'r'))
-linkage_map = list(open(linkage_maps_file, 'r'))
+def extract_linkage_contigs(linkage_maps_file):
+    linkage_contigs = []
+    linkage_map = list(open(linkage_maps_file, 'r'))
+    # Extract all contigs represented in linkage map
+    for line in linkage_map:
+        line = line.replace("\n", '') \
+                .split(",")[0]
 
-old_threshold_contigs = []
-linkage_contigs = []
+        if line not in linkage_contigs:
+            linkage_contigs.append(line)
 
-# Extract all contigs represented in linkage map
-for line in linkage_map:
-    line = line.replace("\n", '') \
-            .split(",")[0]
-
-    if line not in linkage_contigs:
-        linkage_contigs.append(line)
-
-# Extract all contigs represented in threshold .coords file
-for line in threshold_hits:
-    line = line.replace("\n", '') \
-        .split(",")[0] \
-
-    if line not in old_threshold_contigs:  # Only add one of each contig to represented contig list
-        old_threshold_contigs.append(line)
-
-old_threshold_contigs.pop(0)  # Remove header from data
+    return linkage_contigs
 
 
-new_threshold_contigs = []
+def extract_coords_contigs(threshold_hits_file):
+    threshold_hits = list(open(threshold_hits_file, 'r'))
+    old_threshold_contigs = []
+    # Extract all contigs represented in threshold .coords file
+    for line in threshold_hits:
+        line = line.replace("\n", '') \
+            .split(",")[0] \
 
-for entry in old_threshold_contigs:
-    if entry in linkage_contigs:
-        new_threshold_contigs.append(entry)
+        if line not in old_threshold_contigs:  # Only add one of each contig to represented contig list
+            old_threshold_contigs.append(line)
 
-with open('new_common_contig_order.csv', 'w', newline='') as file:
-    x = 0
-    y = 0
-    writer = csv.writer(file)
-    writer.writerow(['position', '.coords contigs', 'linkage contigs'])
+    old_threshold_contigs.pop(0)  # Remove header from data
+    return old_threshold_contigs
 
-    while x < len(new_threshold_contigs):
-        writer.writerow([x + 1, new_threshold_contigs[x], linkage_contigs[x]])
-        if new_threshold_contigs[x] == linkage_contigs[x]:
-            y += 1
-        x += 1
 
-print(linkage_contigs)
-print(new_threshold_contigs)
+def write_to_csv(list_one, list_two, new_file_name):
+    position = 1
+    with open(new_file_name, 'w', newline='') as file:
+        writer = csv.writer(file)
+        for entry in list_one:
+            writer.writerow([position, entry, list_two[position - 1]])
+            position += 1
+
+
+# Extract the data from each file and filter the data to only represent contigs common between both sources
+linkage_contigs = extract_coords_contigs(threshold_hits_file_two)
+threshold_contigs = extract_coords_contigs(threshold_hits_file_three)
+new_threshold_contigs, linkage_contigs = get_common_contigs(threshold_contigs, linkage_contigs)
+
+# Create SequenceMatcher object to compare two contig orders and print results
 sequence_matcher = difflib.SequenceMatcher(None, linkage_contigs, new_threshold_contigs)
 print(f"Similarity in order between updated sorted contigs\nand linkage map contigs: {sequence_matcher.ratio() * 100}%")
 
+# Create a csv with the results of the order comparison
+write_to_csv(linkage_contigs, new_threshold_contigs, 'tx463_Rio_ChineseAmber_comparison.csv')
 
-convert_to_csv('Chinese_Amber_results_onemap.txt', 'ChineseAmber_Linkage_Map.csv')
