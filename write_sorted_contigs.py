c435a6d764fa6d83a75eab13fbcddca073a9c521
cponce-uncc <cristianponce49@gmail.com>
2020-09-04 09:19:21 -0400

    updated to be compatible with new format of output files

--- write_sorted_contigs.py
+++ write_sorted_contigs.py
@@ -7,7 +7,7 @@ import csv
 num_chromosomes = 10
 line_name = 'ChineseAmber'
 contig_file = line_name + ".contigs.fasta"
-sorted_file = line_name + "_sorted.csv"
+sorted_file = "second_approach_contigs.csv"
 output_file = sorted_file.replace('.csv', '.fasta')
 
 # Initialize list of sequence objects based on number of chromosomes in genome
@@ -28,16 +28,21 @@ for contig in SeqIO.parse(contig_file, "fasta"):
 
 x = 1  # Counter variable for printing progress
 
-with open(output_file, "w") as file:
-    for entry in coord_data:
-        contig_id, query_coverage, starting_position, chromosome_num = entry  # Unpack contig data
-        contig_sequence = str(contig_data.get(contig_id))  # Get contig sequence data
-        file.write(f">{contig_id} len={len(contig_sequence)} chr={chromosome_num}\n")  # Write sequence tag and length
-        file.write(contig_sequence + '\n')
+total_bp = 0
+for contig in coord_data:
+    total_bp += len(contig_data.get(contig[0]))
 
-        print(f"CONTIG NO. {x} on CHR. {chromosome_num}, POS. {starting_position} OUT OF {len(coord_data)} CONTIGS")
-        x += 1  # Append counter
-
-    file.close()
+print(total_bp)
+# with open(output_file, "w") as file:
+#     for entry in coord_data:
+#         contig_id, query_coverage, starting_position, chromosome_num = entry  # Unpack contig data
+#         contig_sequence = str(contig_data.get(contig_id))  # Get contig sequence data
+#         file.write(f">{contig_id} len={len(contig_sequence)} chr={chromosome_num}\n")  # Write sequence tag and length
+#         file.write(contig_sequence + '\n')
+#
+#         print(f"CONTIG NO. {x} on CHR. {chromosome_num}, POS. {starting_position} OUT OF {len(coord_data)} CONTIGS")
+#         x += 1  # Append counter
+#
+#     file.close()
 
 print('success')
